5a46344a7a771f13c068b0e9f64b21eab2dab1b4
Guilherme Gonçalves <125297568+guigon14@users.noreply.github.com>
2025-05-30 20:55:38 -0300

    Ajustes infracost

--- terraform-project/modules/ec2/variable.tf
+++ terraform-project/modules/ec2/variable.tf
@@ -1,19 +1,29 @@
-variable "a_zone" {
-    type            = string
-    description     = "Availability Zone"
-    default         = "us-east-1a"
-}
-variable "key_pair_name"{
-    type            = string
-    default         = "terraform_key"
-}
 variable "ami" {
-    type            = string
-    description     = "Ubuntu Server 22.04 LTS"
-    default         = "ami-0e001c9271cf7f3b9"
+  type        = string
+  description = "AMI ID"
 }
-variable "inst_type" {
-    type            = string
-    description     = "1vCPU / 2GB RAM"
-    default         = "t2.small"
+
+variable "a_zone" {
+  type        = string
+  description = "Availability zone"
+}
+
+variable "instance_type_public" {
+  type        = string
+  default     = "t3.small"
+}
+
+variable "instance_type_private" {
+  type        = string
+  default     = "t3.small"
+}
+
+variable "volume_size" {
+  type        = number
+  default     = 30
+}
+
+variable "volume_type" {
+  type        = string
+  default     = "gp3"
 }
