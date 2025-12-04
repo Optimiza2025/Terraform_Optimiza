7a2f82aefddebb209f281daaaa7571a219fd60fb
Guilherme Gonçalves <125297568+guigon14@users.noreply.github.com>
2025-12-03 22:51:28 -0300

    Ajuste sintaxe network.tf

--- terraform-project/modules/network/network.tf
+++ terraform-project/modules/network/network.tf
@@ -2,25 +2,33 @@ resource "aws_vpc" "vpc" {
   cidr_block           = "10.0.0.0/24"
   enable_dns_hostnames = true
   enable_dns_support   = true
-  tags = { Name = "vpc-optimiza" }
+  tags = {
+    Name = "vpc-optimiza"
+  }
 }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
-  tags = { Name = "igw-optimiza" }
+  tags = {
+    Name = "igw-optimiza"
+  }
 }
 
 resource "aws_eip" "nat_eip" {
   vpc        = true
   depends_on = [aws_internet_gateway.igw]
-  tags = { Name = "nat-eip-optimiza" }
+  tags = {
+    Name = "nat-eip-optimiza"
+  }
 }
 
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat_eip.id
   subnet_id     = aws_subnet.public_subnet[0].id
   depends_on    = [aws_internet_gateway.igw]
-  tags = { Name = "nat-optimiza" }
+  tags = {
+    Name = "nat-optimiza"
+  }
 }
 
 # Subnets
@@ -30,7 +38,9 @@ resource "aws_subnet" "public_subnet" {
   cidr_block              = var.public_subnet_cidrs[count.index]
   map_public_ip_on_launch = true
   availability_zone       = var.a_zones[count.index]
-  tags = { Name = "sub-rede-publica-optimiza-${count.index + 1}" }
+  tags = {
+    Name = "sub-rede-publica-optimiza-${count.index + 1}"
+  }
 }
 
 resource "aws_subnet" "private_subnet" {
@@ -38,7 +48,9 @@ resource "aws_subnet" "private_subnet" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = var.private_subnet_cidrs[count.index]
   availability_zone = var.a_zones[count.index]
-  tags = { Name = "sub-rede-privada-optimiza-${count.index + 1}" }
+  tags = {
+    Name = "sub-rede-privada-optimiza-${count.index + 1}"
+  }
 }
 
 # Route Tables
@@ -48,7 +60,9 @@ resource "aws_route_table" "public_rt" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
-  tags = { Name = "rt-public-optimiza" }
+  tags = {
+    Name = "rt-public-optimiza"
+  }
 }
 
 resource "aws_route_table_association" "public" {
@@ -63,7 +77,9 @@ resource "aws_route_table" "private_rt" {
     cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat.id
   }
-  tags = { Name = "rt-private-optimiza" }
+  tags = {
+    Name = "rt-private-optimiza"
+  }
 }
 
 resource "aws_route_table_association" "private" {
@@ -72,46 +88,146 @@ resource "aws_route_table_association" "private" {
   route_table_id = aws_route_table.private_rt.id
 }
 
-# Network ACLs
+# Network ACLs (Corrigidas: Multilinha)
 resource "aws_network_acl" "acl_publica" {
   vpc_id     = aws_vpc.vpc.id
   subnet_ids = aws_subnet.public_subnet[*].id
 
-  # Entradas Permitidas
-  ingress { protocol = "tcp"; rule_no = 100; action = "allow"; cidr_block = "0.0.0.0/0"; from_port = 22; to_port = 22; }   # SSH
-  ingress { protocol = "tcp"; rule_no = 200; action = "allow"; cidr_block = "0.0.0.0/0"; from_port = 80; to_port = 80; }   # HTTP
-  ingress { protocol = "tcp"; rule_no = 300; action = "allow"; cidr_block = "0.0.0.0/0"; from_port = 443; to_port = 443; } # HTTPS
-  
-  # Retorno de tráfego (Ephemeral Ports) - Crucial para não dar timeout
-  ingress { protocol = "tcp"; rule_no = 400; action = "allow"; cidr_block = "0.0.0.0/0"; from_port = 1024; to_port = 65535; }
-  ingress { protocol = "udp"; rule_no = 410; action = "allow"; cidr_block = "0.0.0.0/0"; from_port = 1024; to_port = 65535; }
-  
+  # SSH
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 22
+    to_port    = 22
+  }
+  # HTTP
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 80
+    to_port    = 80
+  }
+  # HTTPS
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 300
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 443
+    to_port    = 443
+  }
+  # Retorno de tráfego TCP (Ephemeral Ports)
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 400
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 1024
+    to_port    = 65535
+  }
+  # Retorno de tráfego UDP (Ephemeral Ports)
+  ingress {
+    protocol   = "udp"
+    rule_no    = 410
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 1024
+    to_port    = 65535
+  }
   # ICMP (Ping)
-  ingress { protocol = "icmp"; rule_no = 450; action = "allow"; cidr_block = "0.0.0.0/0"; from_port = 0; to_port = 0; icmp_type = -1; icmp_code = -1; }
+  ingress {
+    protocol   = "icmp"
+    rule_no    = 450
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+    icmp_type  = -1
+    icmp_code  = -1
+  }
 
   # Saída Total
-  egress { protocol = "-1"; rule_no = 100; action = "allow"; cidr_block = "0.0.0.0/0"; from_port = 0; to_port = 0; }
+  egress {
+    protocol   = "-1"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
 
-  tags = { Name = "acl_publica_optimiza" }
+  tags = {
+    Name = "acl_publica_optimiza"
+  }
 }
 
 resource "aws_network_acl" "acl_privada" {
   vpc_id     = aws_vpc.vpc.id
   subnet_ids = aws_subnet.private_subnet[*].id
 
-  # Entradas Permitidas (Somente Interno da VPC)
-  ingress { protocol = "tcp"; rule_no = 100; action = "allow"; cidr_block = "10.0.0.0/24"; from_port = 22; to_port = 22; }   # SSH Interno
-  ingress { protocol = "tcp"; rule_no = 200; action = "allow"; cidr_block = "10.0.0.0/24"; from_port = 80; to_port = 80; }   # HTTP Interno
-  ingress { protocol = "tcp"; rule_no = 250; action = "allow"; cidr_block = "10.0.0.0/24"; from_port = 3306; to_port = 3306; } # MySQL Interno
-  
-  # Retorno de tráfego (Ephemeral Ports)
-  ingress { protocol = "tcp"; rule_no = 300; action = "allow"; cidr_block = "0.0.0.0/0"; from_port = 1024; to_port = 65535; }
-  ingress { protocol = "udp"; rule_no = 310; action = "allow"; cidr_block = "0.0.0.0/0"; from_port = 1024; to_port = 65535; }
+  # SSH da VPC
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "10.0.0.0/24"
+    from_port  = 22
+    to_port    = 22
+  }
+  # HTTP da VPC
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = "10.0.0.0/24"
+    from_port  = 80
+    to_port    = 80
+  }
+  # MySQL da VPC
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 250
+    action     = "allow"
+    cidr_block = "10.0.0.0/24"
+    from_port  = 3306
+    to_port    = 3306
+  }
+  # Retorno TCP
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 300
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 1024
+    to_port    = 65535
+  }
+  # Retorno UDP
+  ingress {
+    protocol   = "udp"
+    rule_no    = 310
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 1024
+    to_port    = 65535
+  }
 
-  # Saída Total (Para NAT Gateway)
-  egress { protocol = "-1"; rule_no = 100; action = "allow"; cidr_block = "0.0.0.0/0"; from_port = 0; to_port = 0; }
+  # Saída Total
+  egress {
+    protocol   = "-1"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
 
-  tags = { Name = "acl_privada_optimiza" }
+  tags = {
+    Name = "acl_privada_optimiza"
+  }
 }
 
 # Security Groups BASE
@@ -120,12 +236,12 @@ resource "aws_security_group" "sg" {
   description = "SG para EC2s Publicas (Nginx/Bastion)"
   vpc_id      = aws_vpc.vpc.id
 
-  # SSH (Acesso de Admin) - Idealmente troque 0.0.0.0/0 pelo seu IP
+  # SSH (Idealmente restrinja ao seu IP)
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] 
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -134,7 +250,9 @@ resource "aws_security_group" "sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  tags = { Name = "optimiza-public-ec2-sg" }
+  tags = {
+    Name = "optimiza-public-ec2-sg"
+  }
 }
 
 resource "aws_security_group" "mysql_sg" {
@@ -142,12 +260,13 @@ resource "aws_security_group" "mysql_sg" {
   description = "SG para o Banco de Dados MySQL"
   vpc_id      = aws_vpc.vpc.id
 
-
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  tags = { Name = "optimiza-mysql-sg" }
+  tags = {
+    Name = "optimiza-mysql-sg"
+  }
 }
